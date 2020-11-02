c5167dc7d618fd1801e1a2306b227ac5582131fb
Alikhan219 <merabhai786786@gmail.com>
2020-11-02 09:07:01 -0800

    setting up the favicon icon of expense tracker and title

--- src/App.js
+++ src/App.js
@@ -4,7 +4,15 @@ import './App.css';
 function App() {
   return (
     <div >
-     <h1>Hello Ali</h1>
+     <h1>Expense Tracker</h1>
+
+
+
+     <h3>Your Balance <br /> $260</h3>
+     <div className="expense_container">
+     <h3>Income <br /> $260</h3>
+     <h3>Expense <br /> $260</h3>
+     </div>
     </div>
   );
 }
