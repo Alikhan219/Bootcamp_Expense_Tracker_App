14a61e047ebca0f0bad3c976d511dce8ab44f132
Alikhan219 <merabhai786786@gmail.com>
2020-11-03 06:04:24 -0800

    completing css styling

--- src/App.js
+++ src/App.js
@@ -7,24 +7,24 @@ function App() {
       <h1 className="text_center">Expense Tracker</h1>
 
       <h3>
-        Your Balance <br /> $260
+        Your Balance <br /><span className="big-txt"> PKR 260</span>
       </h3>
       <div className="expense_container">
-        <h3>
-          Income <br /> $260
+        <h3 className="IandE">
+          Income <br /> <span className="income-h3"> PKR 260.00</span>
         </h3>
-        <h3>
-          Expense <br /> $260
+        <h3 className="IandE">
+          Expense <br /><span className="expense-h3"> PKR 260.00</span>
         </h3>
       </div>
-      <h3>History</h3>
-      <hr />
-      <h3>Add New Transaction</h3>
+      <h3 className="border">History</h3>
+      
+      <h3 className="bottom">Add New Transaction</h3>
       <br />
       <form className="Transaction_form">
         <div className="form_control">
           <label for="text">Text</label>
-          <br />
+        
           <input type="text" placeholder="Enter text..."/>
         </div>
         <br />
