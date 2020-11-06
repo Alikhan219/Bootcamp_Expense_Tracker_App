941ef8082c656fc9b96e45b9966867e6b16ae152
Alikhan219 <merabhai786786@gmail.com>
2020-11-06 10:01:07 -0800

    adding the total balance method

--- src/Child.js
+++ src/Child.js
@@ -3,72 +3,49 @@ import "./App.css";
 import { TransactionContext } from "./TransContext";
 
 function Child() {
-  let {transactions, addTransaction} = useContext(TransactionContext);
-  let [newDesc, setDesc] = useState("")
-  let [newAmount, setAmount] = useState("")
-  
-   const handleAddition=(event)=>{
-      event.preventDefault();
-      console.log(newDesc, newAmount)
-      addTransaction({
-        amount: newAmount,
-        desc: newDesc
-      })
-   }
+  let { transactions, addTransaction } = useContext(TransactionContext);
+  let [newDesc, setDesc] = useState("");
+  let [newAmount, setAmount] = useState("");
 
+  const handleAddition = (event) => {
+    event.preventDefault();
+    console.log(newDesc, newAmount);
+    addTransaction({
+      amount: Number(newAmount),
+      desc: newDesc,
+    });
+  };
 
-   const getIncome=()=>{
-     let income= 0;
-     for(let i=0; i< transactions.length; i++){
-       if(transactions[i]>0){
-         income += transactions.amount
-       }
-       return income;
-     }
-   }
-   const getExpense=()=>{
-    let expense= 0;
-    for(let i=0; i< transactions.length; i++){
-      if(transactions[i]>0){
-        expense += transactions.amount
-      }
-      return expense;
+  const getIncome = () => {
+    let income = 0;
+    for (var i = 0; i < transactions.length; i++) {
+      if (transactions[i].amount > 0) income += transactions[i].amount;
     }
-  }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return income;
+  };
+  const getExpense = () => {
+    let expense = 0;
+    for (var i = 0; i < transactions.length; i++) {
+      if (transactions[i].amount > 0) expense += transactions[i].amount;
+    }
+    return expense;
+  };
 
   return (
     <div className="container">
       <h1 className="text_center">Expense Tracker</h1>
-
       <h3>
         Your Balance <br />
-        <span className="big-txt"> PKR 260.00</span>
+        <span className="big-txt"> {getIncome() + getExpense()}</span>
       </h3>
       <div className="expense_container">
         <h3 className="IandE">
-          Income <br /> <span className="income-h3">PKR 360.00</span>
+          Income <br /> <span className="income-h3">{getIncome()}</span>
         </h3>
         <h3 className="IandE">
           Expense <br />
-          <span className="expense-h3"> PKR 260.00</span>
+          <span className="expense-h3"> {getExpense()}</span>
         </h3>
       </div>
       <h3 className="border">History</h3>
@@ -89,7 +66,14 @@ function Child() {
         <div className="form_control">
           <label for="text">Text</label>
 
-          <input name="thing" type="text" placeholder="Enter text..." onChange={(ev)=>setDesc(ev.target.value)} required/>
+          <input
+            autoComplete="off"
+            name="thing"
+            type="text"
+            placeholder="Enter text..."
+            onChange={(ev) => setDesc(ev.target.value)}
+            required
+          />
         </div>
         <br />
         <div className="form_control">
@@ -98,7 +82,13 @@ function Child() {
             <br />
             (negative - expense, positive - income)
           </label>
-          <input name="amounts" type="number" placeholder="Enter amount..." onChange={(ev)=>setAmount(ev.target.value)} required/>
+          <input
+            name="amounts"
+            type="number"
+            placeholder="Enter amount..."
+            onChange={(ev) => setAmount(ev.target.value)}
+            required
+          />
         </div>
         <br />
         <button className="btn">Add Transaction</button>
